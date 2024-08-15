bc50e0959f530dc1103f28e595665a5b22112423
rafliakmalg <158143597+rafliakmalg@users.noreply.github.com>
2024-08-15 08:57:13 +0700

    Redirect

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/', function () {
+    return redirect('/users');
+});
+
 Route::resource('users', UserController::class);
